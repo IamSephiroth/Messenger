4b6d4f56b7a86fdd59416c8b539ebcdd7a267d26
Ellie Woods <esw717@cs.bham.ac.uk>
2020-03-11 10:44:27 +0000

    Upload New File

---
+++ NewPostgreSQLJDBC5.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class NewPostgreSQLJDBC5 {
+   public static void main( String args[] ) {
+      Connection c = null;
+      Statement stmt = null;
+      try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk/",
+            		"krishna", "08tgfexbf8");
+         c.setAutoCommit(false);
+         System.out.println("Opened database successfully");
+
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery( "SELECT * FROM MESSAGES;" );
+         while ( rs.next() ) {
+            String id = rs.getString("id");
+            String receiver_id = rs.getString("receiver_id");
+            String message  = rs.getString("message");
+            String date = rs.getString("date");
+            System.out.println( id +"  |  "+ receiver_id +"  |  "+ message +"  |  "+ date );
+           
+            System.out.println();
+         }
+         rs.close();
+         stmt.close();
+         c.close();
+      } catch ( Exception e ) {
+         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+         System.exit(0);
+      }
+      System.out.println("Operation done successfully");
+   }
+}
