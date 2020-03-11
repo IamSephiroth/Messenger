e8792c344a3b322e9e6539ae8a1c9a3302ed9ede
Ellie Woods <esw717@cs.bham.ac.uk>
2020-03-11 14:24:59 +0000

    updated database

--- PostgreSQLJDBC.java
+++ PostgreSQLJDBC.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 
-public class NewPostgreSQLJDBC {
+public class PostgreSQLJDBC {
    public static void main( String args[] ) {
       Connection c = null;
       Statement stmt = null;
