286181c1d7e6eda2bcf6994e21e88940fca2dd78
MIYO Takahashi <mxt935@student.bham.ac.uk>
2020-03-13 10:13:34 +0000

    Added ArrayList of ClientManager to handle senders and destination.

--- Server.java
+++ Server.java
@@ -1,63 +1,118 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.HashSet;
-//import java.util.Map;
-//import java.util.Set;
-//import java.util.TreeSet;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * This class contains the main method.
+ * 	 * accept connevtion from client and pass th econnection to the ClientManager.
+ * 
+ *
+ */
 public class Server {
-	
 //	private Set<Socket> clients = new HashSet<Socket>();
 //	private Map<String, Socket> clients = new HashMap<String, Socket>();
-	
-	static Vector<ServerThread> Users = new Vector<>(); // vector to store clients that are connected to the server
-	
-	static int counter = 0; // counter for clients
-	static Account user;
-	
-	static ArrayList<String> accountName = new ArrayList<String>();
-	static ArrayList<ObjectOutputStream> chatLog = new ArrayList<ObjectOutputStream>();
 	
     /**
 	 * Setup a ServerSocket in the Server class.
 	 * 
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ServerSocket ss = null;
+		ObjectOutputStream toClient;
+	    ObjectInputStream fromClient;
+		Socket client = null;
+		// List of currently online clients.
+		//ArrayList<Socket> clients = new ArrayList<Socket>();
+		// ClientManager know about client sockets as well.
+		ArrayList<ClientManager> clientManagers = new ArrayList<ClientManager>();
+		
+			ServerSocket serverSocket = null;
 		try {
-			ss = new ServerSocket(50000);
-			System.out.println("Server is listening on port 50000");
+			/*
+			 *  Instantiate ServerSocket object.
+			 */
+			serverSocket = new ServerSocket(50000);
 			System.out.println("Waiting for a client.");
+			
+			//acceptClients();
 		} catch (IOException e) {
 			System.err.println("could not listen on port: " + 50000);
 		}
-
 		try {
 			while (true) {
-				Socket clientSocket = ss.accept();
+				/*
+				 *  Accept connection from a client.
+				 *  ClientSocket is a unique socket of the accepted client.
+				 */
+				Socket clientSocket = serverSocket.accept();
+				/*
+				 *  Add client to the list of clients currently online.
+				 */
+				// clients.add(clientSocket);
 				System.out.println("Client accepted");
-							
-				ServerThread chatThread = new ServerThread(clientSocket, user); // new handler created for each client
-				Thread clients = new Thread(chatThread);
-				Users.add(chatThread);
-				System.out.println("Client added to active users list");
-				clients.start();
-				counter++;
+				/*
+				 *  Construct a object of ClientManager class with the newly
+				 *  accepted client.
+				 */
+				ClientManager s = new ClientManager(clientSocket);
+				clientManagers.add(s);
+				/*
+				 *  Construct a thread with the runnable object.
+				 */
+				Thread a = new Thread(s);
+				/*
+				 *  Execute the run method in ClientManager class.
+				 */
+				a.start();
+				/*
+				 * Handle sender and receiver.
+				 * Listen to message form server and check which account the thread handles.
+				 * Send the message to all other objects of ClientManager.
+				 */
 				
 			}
 		} catch (Exception e) {
 				try {
-					ss.close();
+					/*
+					 *  Close the connection from server end.
+					 */
+					serverSocket.close();
 				} catch (IOException io) {
 					System.out.println("Could not close serverSocket" + io.getMessage());
 				}
 			}
 		}
-	}
+	
+	/**
+	 * Miyo added
+	 */
+//	public static Server instance;
+//	private ArrayList<ChatRoom> activeRooms;
+//	private ArrayList<Client> activeUsers;
+//	/**
+//	 * Returns Server instance, generate one if it does not exist.
+//	 * 
+//	 * @return
+//	 */
+//	public static Server getInstance() {
+//		if (instance == null) {
+//			instance = new Server();
+//		}
+//		return instance;
+//	}
+//	
+//	public Server() {
+//		activeRooms = new ArrayList<ChatRoom>();
+//		activeUsers = new ArrayList<Client>();
+//	}
+}
+
 	/**
 	 * Wait for a client to connect. 
 	 */
